60e0c0417bc0993724e780f74663e23850cbf762
kelvin-lu <kelvinlu@mit.edu>
2019-05-22 00:40:33 +0000

    Add new feature

---
+++ ballet_ames_sim_lower_threshold/features/contrib/user_03/feature_11.py
@@ -0,0 +1,13 @@
+from ballet import Feature
+import ballet.eng
+import sklearn.impute
+import sklearn.preprocessing
+
+
+input = ["BsmtFin Type 1"]
+transformer = [
+    ballet.eng.missing.NullFiller(replacement="None"),
+    sklearn.preprocessing.OneHotEncoder(),
+]
+name = "Rating of basement finished area type"
+feature = Feature(input=input, transformer=transformer, name=name)
